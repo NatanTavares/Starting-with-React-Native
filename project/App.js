5999bd61fcb3793035257abbcf737b61be4bc50a
Natan <nattantavares.s15@gmail.com>
2020-10-10 23:46:35 -0300

    The first component is saying Hello World

--- project/App.js
+++ project/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import First from './src/components/First';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <First />
       <StatusBar style="auto" />
     </View>
   );
