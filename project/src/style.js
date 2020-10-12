567c4696403252afdd3fd8970a68a74493dfd6ee
Natan <nattantavares.s15@gmail.com>
2020-10-11 23:22:24 -0300

    Created the global style sheet

---
+++ project/src/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  largerText: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: '#fff',
+  },
+  regularText: {
+    fontSize: 18,
+    color: '#fff',
+  },
+  smallText: {
+    fontSize: 12,
+    color: '#fff',
+  },
+});
