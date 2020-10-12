47be107072fa0a2acd9a402fdc6bdbfabe56d8b2
Natan <nattantavares.s15@gmail.com>
2020-10-11 22:57:35 -0300

    Organizing Components (Multi Components)

--- project/App.js
+++ project/App.js
@@ -3,12 +3,19 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import First from './src/components/First';
+import CompDefault, { Comp1, Comp2 } from './src/components/Multi';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <First />
       <StatusBar style="auto" />
+
+      <First />
+
+      <CompDefault />
+      <Comp1 />
+      <Comp2 />
+
     </View>
   );
 }
---
+++ project/src/components/Multi.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default () => {
+  return (
+    <Text>Comp's works!</Text>
+  );
+}
+
+export function Comp1() {
+  return (
+    <Text>Comp1's works!</Text>
+  );
+}
+
+export function Comp2() {
+  return (
+    <Text>Comp2's works!</Text>
+  );
+}
